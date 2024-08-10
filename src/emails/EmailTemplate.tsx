f0a570d2feb05ba4cd6fb8386296a17e761ae030
badAl <badalsingh120403@gmail.com>
2024-08-10 10:01:32 +0530

    feat: initialize send-email template

---
+++ src/emails/EmailTemplate.tsx
@@ -0,0 +1,9 @@
+const EmailTemplate = () => {
+  return (
+    <h1 className="text-5xl bg-gray-400 text-center p-10 rounded-xl shadow-md shadow-gray-400 m-4 border-solid border-2 border-gray-300 ">
+      Email: al@al.com
+    </h1>
+  );
+};
+
+export default EmailTemplate;
